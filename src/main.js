8dfaa327d6c0ea26b61235689b715218524e2898
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-03-09 16:57:03 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import { example, filterData } from './data.js';
+import data from './data/lol/lol.js';
+
+// import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+filterData(data.data, 0, 0);
