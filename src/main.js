955eba948e9ea46469118e3672aa2028e6408f53
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-03-09 17:15:15 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,3 +6,14 @@ import data from './data/lol/lol.js';
 
 console.log(example, data);
 filterData(data.data, 0, 0);
+
+
+function viewCuad(dataSet){
+let container = document.getElementById('cuad')
+//Toma un set de datos y los muestra en una cuadrícula
+for( const champion in dataSet) {
+let image = document.createElement('img');
+image.setAttribute('src', dataSet[champion].img)
+container.appendChild(image);
+}
+}
