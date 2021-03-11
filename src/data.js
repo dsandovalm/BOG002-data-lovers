119ccfd441ef963ef815f2486727b88fab764c0d
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-03-11 16:34:54 -0500

    Update data.js

--- src/data.js
+++ src/data.js
@@ -7,10 +7,15 @@ export const anotherExample = () => {
 };
 
 export const filterData = (data,condition) => {
-  //Data es un objeto
+  //Data es un objeto. Los campeones son los atributos de este objeto
   //Condition es un arreglo de condiciones
   let filter = {};
   for( const champion in data){
+    //Recorrer el objeto
+    condition.forEach( c => {
+      //Recorrer el arreglo
+      console.log(c.key);
+    });
     console.log(`${champion}`);
   }
-}
+};
