05cd3ed33609ab6d54d0967a1a942899eade1bba
dsandovalm <58996746+dsandovalm@users.noreply.github.com>
2021-04-12 09:53:39 -0500

    Update data.spec.js

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,49 @@
-import { example, anotherExample } from '../src/data.js';
+import {filterData, sortData, searchData } from '../src/data.js';
+import data from "../src/data/lol/lol.js";
 
-
-describe('example', () => {
+describe('filterData', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof filterData).toBe('function');
   });
 
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
+  it('returns an object', () => {
+    expect(typeof(filterData(data.data,{tags:'Support',info:{defense:4},partype:'MP'}))).toBe('object');
   });
 });
 
-
-describe('anotherExample', () => {
+describe('sortData', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof sortData).toBe('function');
   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+  it('returns an object', () => {
+    expect(typeof(sortData(data.data,'magic'))).toBe('object');
+  });
+
+  it('returns an object', () => {
+    expect(typeof(sortData(data.data,'armor'))).toBe('object');
+  });
+
+  it('returns an object', () => {
+    expect(typeof(sortData(data.data,'tags'))).toBe('object');
+  });
+
+  it('returns all the data', () => {
+    expect(sortData(data.data,'id')).toStrictEqual(data.data);
+  });
+
+});
+
+describe('searchData', () => {
+  it('is a function', () => {
+    expect(typeof searchData).toBe('function');
+  });
+
+  it('returns an object', () => {
+    expect(typeof(searchData(data.data,'strong warrior'))).toBe('object');
+  });
+
+  it('returns the champion', () => {
+    expect(searchData(data.data,"Akali")).toStrictEqual({Akali: data.data['Akali']});
   });
 });
